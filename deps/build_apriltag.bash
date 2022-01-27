44d03b4709316a9c2cd779a114bd1cb9b5068e1c
Chris Choi <chutsu@gmail.com>
2022-01-27 22:54:13 +0000

    Fix apriltag install script

--- deps/build_apriltag.bash
+++ deps/build_apriltag.bash
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -e
-BUILD_TYPE=Release
-INSTALL_PREFIX=${PWD}
+
+# Clone
+if [ ! -d src/apriltag ]; then
+  cd src
+  git clone https://github.com/chutsu/apriltag
+  cd ..
+fi
 
 # Build
 cd src/apriltag
-rm -rf build
-mkdir -p build
-cd build
-cmake .. \
-  -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-  -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
-  -DCMAKE_PREFIX_PATH=$INSTALL_PREFIX
-make
+make deps
+make build
 make install

cb5f97641664e368e76d734e28d306d3395b0126
Chris Choi <chutsu@gmail.com>
2022-02-12 22:44:41 +0000

    Add test data

--- yac/tests/test_calib_vi.cpp
+++ yac/tests/test_calib_vi.cpp
@@ -1,71 +1,87 @@
 #include "munit.hpp"
-#include "util/euroc.hpp"
 #include "calib_vi.hpp"
 
 namespace yac {
 
+#ifndef TEST_PATH
+#define TEST_PATH "."
+#endif
+
+#define TEST_IMUCAM_DATA TEST_PATH "/test_data/calib/imu_april"
+
+void load_imu_data(const std::string &csv_path,
+                   timestamps_t &timestamps,
+                   vec3s_t &a_B,
+                   vec3s_t &w_B) {
+  // Open file for loading
+  int nb_rows = 0;
+  FILE *fp = file_open(csv_path, "r", &nb_rows);
+  if (fp == nullptr) {
+    FATAL("Failed to open [%s]!", csv_path.c_str());
+  }
+
+  // Parse file
+  for (int i = 0; i < nb_rows; i++) {
+    // Skip first line
+    if (i == 0) {
+      skip_line(fp);
+      continue;
+    }
+
+    // Parse line
+    timestamp_t ts = 0;
+    double w_x, w_y, w_z = 0.0;
+    double a_x, a_y, a_z = 0.0;
+    int retval = fscanf(fp,
+                        "%" SCNu64 ",%lf,%lf,%lf,%lf,%lf,%lf",
+                        &ts,
+                        &w_x,
+                        &w_y,
+                        &w_z,
+                        &a_x,
+                        &a_y,
+                        &a_z);
+    if (retval != 7) {
+      FATAL("Failed to parse line in [%s]", csv_path.c_str());
+    }
+
+    timestamps.push_back(ts);
+    w_B.emplace_back(w_x, w_y, w_z);
+    a_B.emplace_back(a_x, a_y, a_z);
+  }
+  fclose(fp);
+}
+
 timeline_t setup_test_data() {
-  // Load calibration data
-  const calib_target_t target{"aprilgrid", 6, 6, 0.088, 0.3};
-  const std::string data_path = "/data/euroc/imu_april";
-  std::map<int, std::string> grid_paths = {
-      {0, data_path + "/grid0/cam0"},
-      {1, data_path + "/grid0/cam1"},
-  };
-  LOG_INFO("Loading EuRoC data [%s]", data_path.c_str());
+  // Load grid data
+  timeline_t timeline;
 
-  // Preprocess aprilgrids
-  LOG_INFO("Preprocessing AprilGrid data ...");
-  aprilgrid_detector_t detector{6, 6, 0.088, 0.3};
-  for (int cam_idx = 0; cam_idx < 2; cam_idx++) {
-    if (system(("mkdir -p " + grid_paths[cam_idx]).c_str()) != 0) {
-      FATAL("Failed to create dir [%s]", grid_paths[cam_idx].c_str());
-    }
+  // -- Add cam0 grids
+  std::vector<std::string> cam0_files;
+  list_files(TEST_IMUCAM_DATA "/cam0", cam0_files);
+  for (auto grid_path : cam0_files) {
+    grid_path = std::string(TEST_IMUCAM_DATA "/cam0/") + grid_path;
+    aprilgrid_t grid(grid_path);
+    timeline.add(grid.timestamp, 0, grid);
   }
 
-  // Loop through timeline
-  euroc_calib_t calib_data{data_path};
-  auto timeline = calib_data.timeline();
-  size_t i = 0;
-
-  for (const auto &ts : timeline.timestamps) {
-    if (i++ % 100 == 0) {
-      printf(".");
-      fflush(stdout);
-    }
-
-    for (const auto &event : timeline.get_events(ts)) {
-      if (auto cam_event = dynamic_cast<camera_event_t *>(event)) {
-        // Setup
-        const timestamp_t ts = cam_event->ts;
-        const int cam_idx = cam_event->cam_idx;
-        const auto flag = cv::IMREAD_GRAYSCALE;
-        const cv::Mat image = cv::imread(cam_event->img_path, flag);
-        const std::string grid_fname = std::to_string(ts) + ".csv";
-        const std::string grid_file = grid_paths[cam_idx] + "/" + grid_fname;
-
-        // Detect or load
-        aprilgrid_t grid;
-        if (file_exists(grid_file) == false) {
-          grid = detector.detect(ts, image);
-          grid.save(grid_file);
-        } else {
-          grid.load(grid_file);
-          if (grid.detected == false) {
-            grid.timestamp = ts;
-            grid.tag_rows = detector.tag_rows;
-            grid.tag_cols = detector.tag_cols;
-            grid.tag_size = detector.tag_size;
-            grid.tag_spacing = detector.tag_spacing;
-          }
-        }
-
-        // Add aprilgrid to timeline
-        timeline.add(ts, cam_idx, grid);
-      }
-    }
+  // -- Add cam1 grids
+  std::vector<std::string> cam1_files;
+  list_files(TEST_IMUCAM_DATA "/cam1", cam1_files);
+  for (auto grid_path : cam1_files) {
+    grid_path = std::string(TEST_IMUCAM_DATA "/cam1/") + grid_path;
+    aprilgrid_t grid(grid_path);
+    timeline.add(grid.timestamp, 1, grid);
   }
-  printf("\n");
+
+  // -- Add imu events
+  timestamps_t imu_ts;
+  vec3s_t imu_acc;
+  vec3s_t imu_gyr;
+  load_imu_data(TEST_IMUCAM_DATA "/imu0.csv", imu_ts, imu_acc, imu_gyr);
+  for (size_t k = 0; k < imu_ts.size(); k++) {
+    timeline.add(imu_ts[k], imu_acc[k], imu_gyr[k]);
+  }
 
   return timeline;
 }
@@ -142,30 +158,30 @@ int test_calib_vi() {
   mat4_t T_C0C1;
   mat4_t T_BC0;
   mat4_t T_BC1;
-  // -- Custom calibration values
-  config_t config{"/tmp/calib-results.yaml"};
-  veci2_t cam_res;
-  parse(config, "cam0.resolution", cam_res);
-  parse(config, "cam0.proj_params", cam0_proj_params);
-  parse(config, "cam0.dist_params", cam0_dist_params);
-  parse(config, "cam1.proj_params", cam1_proj_params);
-  parse(config, "cam1.dist_params", cam1_dist_params);
-  parse(config, "T_cam0_cam1", T_C0C1);
+  // // -- Custom calibration values
+  // config_t config{"/tmp/calib-results.yaml"};
+  // veci2_t cam_res;
+  // parse(config, "cam0.resolution", cam_res);
+  // parse(config, "cam0.proj_params", cam0_proj_params);
+  // parse(config, "cam0.dist_params", cam0_dist_params);
+  // parse(config, "cam1.proj_params", cam1_proj_params);
+  // parse(config, "cam1.dist_params", cam1_dist_params);
+  // parse(config, "T_cam0_cam1", T_C0C1);
   // -- Euroc Calibration values
-  // cam0_proj_params << 458.654, 457.296, 367.215, 248.375;
-  // cam0_dist_params << -0.28340811, 0.07395907, 0.00019359, 1.76187114e-05;
-  // cam1_proj_params << 457.587, 456.134, 379.999, 255.238;
-  // cam1_dist_params << -0.28368365, 0.07451284, -0.00010473, -3.555e-05;
+  cam0_proj_params << 458.654, 457.296, 367.215, 248.375;
+  cam0_dist_params << -0.28340811, 0.07395907, 0.00019359, 1.76187114e-05;
+  cam1_proj_params << 457.587, 456.134, 379.999, 255.238;
+  cam1_dist_params << -0.28368365, 0.07451284, -0.00010473, -3.555e-05;
   // clang-format off
-  // T_BC0 << 0.0148655429818, -0.999880929698, 0.00414029679422, -0.0216401454975,
-  //         0.999557249008, 0.0149672133247, 0.025715529948, -0.064676986768,
-  //         -0.0257744366974, 0.00375618835797, 0.999660727178, 0.00981073058949,
-  //         0.0, 0.0, 0.0, 1.0;
-  // T_BC1 << 0.0125552670891, -0.999755099723, 0.0182237714554, -0.0198435579556,
-  //         0.999598781151, 0.0130119051815, 0.0251588363115, 0.0453689425024,
-  //         -0.0253898008918, 0.0179005838253, 0.999517347078, 0.00786212447038,
-  //         0.0, 0.0, 0.0, 1.0;
-  // T_C0C1 = T_BC0.inverse() * T_BC1; // Camera-Camera extrinsics
+  T_BC0 << 0.0148655429818, -0.999880929698, 0.00414029679422, -0.0216401454975,
+          0.999557249008, 0.0149672133247, 0.025715529948, -0.064676986768,
+          -0.0257744366974, 0.00375618835797, 0.999660727178, 0.00981073058949,
+          0.0, 0.0, 0.0, 1.0;
+  T_BC1 << 0.0125552670891, -0.999755099723, 0.0182237714554, -0.0198435579556,
+          0.999598781151, 0.0130119051815, 0.0251588363115, 0.0453689425024,
+          -0.0253898008918, 0.0179005838253, 0.999517347078, 0.00786212447038,
+          0.0, 0.0, 0.0, 1.0;
+  T_C0C1 = T_BC0.inverse() * T_BC1; // Camera-Camera extrinsics
   // clang-format on
   T_BC0 = I(4);   // Set cam0 as body frame
   T_BC1 = T_C0C1; // Cam1 extrinsics
@@ -362,7 +378,7 @@ void test_suite() {
   MU_ADD_TEST(test_calib_vi_add_imu);
   MU_ADD_TEST(test_calib_vi_add_camera);
   MU_ADD_TEST(test_calib_vi);
-  MU_ADD_TEST(test_marg_error);
+  // MU_ADD_TEST(test_marg_error);
 }
 
 } // namespace yac

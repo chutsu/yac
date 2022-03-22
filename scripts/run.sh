55d371256f994706b6a27e479739c3d5e1b4c42c
Chris Choi <chutsu@gmail.com>
2022-03-22 09:46:17 +0000

    Update run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,92 +6,90 @@ set -e
 # --prefix 'gdb -ex run -args'
 
 # YAC-UTIL
-# RUN_CMD="test_util_aprilgrid"
-# RUN_CMD="test_util_aprilgrid --target test_aprilgrid_profile"
-# RUN_CMD="test_util_config"
-# RUN_CMD="test_util_cv"
-# RUN_CMD="test_util_core --target test_ctraj_sandbox"
-# RUN_CMD="test_util_data"
-# RUN_CMD="test_util_fs"
-# RUN_CMD="test_util_net"
-# RUN_CMD="test_util_sim"
-# RUN_CMD="test_util_timeline"
+# RUN_CMD="./test_util_aprilgrid"
+# RUN_CMD="./test_util_aprilgrid --target test_aprilgrid_profile"
+# RUN_CMD="./test_util_config"
+# RUN_CMD="./test_util_cv"
+# RUN_CMD="./test_util_core"
+# RUN_CMD="./test_util_data"
+# RUN_CMD="./test_util_fs"
+# RUN_CMD="./test_util_net"
+# RUN_CMD="./test_util_sim"
+# RUN_CMD="./test_util_timeline"
 
 # YAC - CALIBRATION DATA
-# RUN_CMD="test_calib_data"
+# RUN_CMD="./test_calib_data"
 
 # YAC - RESIDUALS
-# RUN_CMD="test_calib_residuals"
-# RUN_CMD="test_calib_residuals --target test_pose_residual"
-# RUN_CMD="test_calib_residuals --target test_reproj_residual"
-# RUN_CMD="test_calib_residuals --target test_fiducial_residual"
-# RUN_CMD="test_calib_residuals --target test_imu_residual"
+# RUN_CMD="./test_calib_residuals"
+# RUN_CMD="./test_calib_residuals --target test_prior"
+# RUN_CMD="./test_calib_residuals --target test_pose_prior"
+# RUN_CMD="./test_calib_residuals --target test_reproj_residual"
+# RUN_CMD="./test_calib_residuals --target test_fiducial_residual"
+# RUN_CMD="./test_calib_residuals --target test_imu_residual"
 
 # YAC - CAMERA CALIBRATION
-# RUN_CMD="test_calib_camera"
-# RUN_CMD="test_calib_camera --target test_calib_view"
-# RUN_CMD="test_calib_camera --target test_calib_camera_add_camera_data"
-# RUN_CMD="test_calib_camera --target test_calib_camera_add_camera"
-# RUN_CMD="test_calib_camera --target test_calib_camera_add_camera_extrinsics"
-# RUN_CMD="test_calib_camera --target test_calib_camera_add_pose"
-# RUN_CMD="test_calib_camera --target test_calib_camera_add_and_remove_view"
-# RUN_CMD="test_calib_camera --target test_calib_camera_find_nbv"
-# RUN_CMD="test_calib_camera --target test_calib_camera_filter_all_views"
-# RUN_CMD="test_calib_camera --target test_calib_camera_remove_all_views"
-# RUN_CMD="test_calib_camera --target test_calib_camera_remove_outliers"
-# RUN_CMD="test_calib_camera --target test_calib_camera_solve_batch"
-# RUN_CMD="test_calib_camera --target test_calib_camera_solve_inc"
-# RUN_CMD="test_calib_camera --target test_calib_camera_mono"
-RUN_CMD="test_calib_camera --target test_calib_camera_stereo"
-# RUN_CMD="test_calib_camera --target test_marg_residual"
-# RUN_CMD="calib_euroc"
-# RUN_CMD="calib_inspect '/home/chutsu/projects/yac/yac/configs/calib-euroc.yaml' /data/euroc/cam_april"
-# RUN_CMD="calib_inspect '/home/chutsu/projects/yac/yac/configs/calib-kalibr.yaml' /data/euroc/imu_april"
-# RUN_CMD="calib_inspect '/home/chutsu/projects/yac/yac/configs/calib-yac.yaml' /data/euroc/imu_april"
-# RUN_CMD="calib_inspect '/data/euroc_results/configs/euroc/euroc.yaml' /data/euroc/cam_april"
-# RUN_CMD="calib_sandbox '/tmp/yac_data/calib.yaml'"
-# RUN_CMD="rosrun --prefix 'gdb -ex run -args' yac test_calib_camera --target test_marg_residual"
-# RUN_CMD="rosrun --prefix 'gdb -ex run -args' yac test_calib_camera"
-# RUN_CMD="rosrun --prefix 'gdb -ex run -args' yac test_calib_camera --target test_calib_camera_kalibr_data"
+# RUN_CMD="./test_calib_camera"
+# RUN_CMD="./test_calib_camera --target test_calib_view"
+# RUN_CMD="./test_calib_camera --target test_calib_camera_add_camera_data"
+# RUN_CMD="./test_calib_camera --target test_calib_camera_add_camera"
+# RUN_CMD="./test_calib_camera --target test_calib_camera_add_camera_extrinsics"
+# RUN_CMD="./test_calib_camera --target test_calib_camera_add_pose"
+# RUN_CMD="./test_calib_camera --target test_calib_camera_add_and_remove_view"
+# RUN_CMD="./test_calib_camera --target test_calib_camera_find_nbv"
+# RUN_CMD="./test_calib_camera --target test_calib_camera_filter_all_views"
+# RUN_CMD="./test_calib_camera --target test_calib_camera_remove_all_views"
+# RUN_CMD="./test_calib_camera --target test_calib_camera_remove_outliers"
+# RUN_CMD="./test_calib_camera --target test_calib_camera_solve_batch"
+# RUN_CMD="./test_calib_camera --target test_calib_camera_solve_inc"
+# RUN_CMD="./test_calib_camera --target test_calib_camera_mono"
+# RUN_CMD="./test_calib_camera --target test_calib_camera_stereo"
+# RUN_CMD="./test_calib_camera --target test_marg_residual"
+RUN_CMD="./calib_euroc"
+# RUN_CMD="./calib_inspect '/home/chutsu/projects/yac/yac/configs/calib-euroc.yaml' /data/euroc/cam_april"
+# RUN_CMD="./calib_inspect '/home/chutsu/projects/yac/yac/configs/calib-kalibr.yaml' /data/euroc/imu_april"
+# RUN_CMD="./calib_inspect '/home/chutsu/projects/yac/yac/configs/calib-yac.yaml' /data/euroc/cam_april"
+# RUN_CMD="./calib_inspect '/data/euroc_results/configs/euroc/euroc.yaml' /data/euroc/cam_april"
+# RUN_CMD="./calib_sandbox '/tmp/yac_data/calib.yaml'"
 
 
 # YAC - MOCAP CALIBRATION
 # RUN_CMD="test_calib_mocap"
 
 # YAC - VISUAL-INERTIAL CALIBRATION
-# RUN_CMD="test_calib_vi"
-# RUN_CMD="test_calib_vi --target test_calib_vi"
-# RUN_CMD="test_calib_vi --target test_calib_vi_batch"
-# RUN_CMD="test_calib_vi --target test_calib_vi_online"
-# RUN_CMD="test_calib_vi --target test_calib_vi_copy_constructor"
+# RUN_CMD="./test_calib_vi"
+# RUN_CMD="./test_calib_vi --target test_calib_vi"
+# RUN_CMD="./test_calib_vi --target test_calib_vi_batch"
+# RUN_CMD="./test_calib_vi --target test_calib_vi_online"
+# RUN_CMD="./test_calib_vi --target test_calib_vi_copy_constructor"
 
 # YAC - NBT
-# RUN_CMD="test_calib_nbt"
-# RUN_CMD="test_calib_nbt --target test_nbt_orbit_trajs"
-# RUN_CMD="test_calib_nbt --target test_nbt_pan_trajs"
-# RUN_CMD="test_calib_nbt --target test_nbt_figure8_trajs"
-# RUN_CMD="test_calib_nbt --target test_simulate_cameras"
-# RUN_CMD="test_calib_nbt --target test_simulate_imu"
-# RUN_CMD="test_calib_nbt --target test_nbt_eval"
-# RUN_CMD="test_calib_nbt --target test_nbt_find"
+# RUN_CMD="./test_calib_nbt"
+# RUN_CMD="./test_calib_nbt --target test_nbt_orbit_trajs"
+# RUN_CMD="./test_calib_nbt --target test_nbt_pan_trajs"
+# RUN_CMD="./test_calib_nbt --target test_nbt_figure8_trajs"
+# RUN_CMD="./test_calib_nbt --target test_simulate_cameras"
+# RUN_CMD="./test_calib_nbt --target test_simulate_imu"
+# RUN_CMD="./test_calib_nbt --target test_nbt_eval"
+# RUN_CMD="./test_calib_nbt --target test_nbt_find"
 
 # YAC - NBV
-# RUN_CMD="test_calib_nbv"
-# RUN_CMD="test_calib_nbv --target test_calib_target_origin"
-# RUN_CMD="test_calib_nbv --target test_calib_init_poses"
-# RUN_CMD="test_calib_nbv --target test_calib_nbv_poses"
-# RUN_CMD="test_calib_nbv --target test_nbv_draw"
-# RUN_CMD="test_calib_nbv --target test_nbv_test_grid"
-# RUN_CMD="test_calib_nbv --target test_nbv_find"
-# RUN_CMD="test_calib_nbv --target test_calib_orbit_trajs"
-# RUN_CMD="test_calib_nbv --target test_calib_pan_trajs"
-# RUN_CMD="test_calib_nbv --target test_simulate_cameras"
-# RUN_CMD="test_calib_nbv --target test_simulate_imu"
-# RUN_CMD="test_calib_nbv --target test_nbt_eval_traj"
+# RUN_CMD="./test_calib_nbv"
+# RUN_CMD="./test_calib_nbv --target test_calib_target_origin"
+# RUN_CMD="./test_calib_nbv --target test_calib_init_poses"
+# RUN_CMD="./test_calib_nbv --target test_calib_nbv_poses"
+# RUN_CMD="./test_calib_nbv --target test_nbv_draw"
+# RUN_CMD="./test_calib_nbv --target test_nbv_test_grid"
+# RUN_CMD="./test_calib_nbv --target test_nbv_find"
+# RUN_CMD="./test_calib_nbv --target test_calib_orbit_trajs"
+# RUN_CMD="./test_calib_nbv --target test_calib_pan_trajs"
+# RUN_CMD="./test_calib_nbv --target test_simulate_cameras"
+# RUN_CMD="./test_calib_nbv --target test_simulate_imu"
+# RUN_CMD="./test_calib_nbv --target test_nbt_eval_traj"
 
 # YAC - MARG RESIDUAL
-# RUN_CMD="test_marg_residual"
-# RUN_CMD="test_marg_residual --target test_marg_block"
+# RUN_CMD="./test_marg_residual"
+# RUN_CMD="./test_marg_residual --target test_marg_block"
 
 # YAC - ROS NODES
 # RUN_CMD="roslaunch yac_ros record_camera.launch"
@@ -116,9 +114,7 @@ tmux send-keys -t dev -R C-l C-m
 tmux send-keys -t dev -R "\
 cd ~/projects/yac \
 && make lib \
-&& make release \
-# && source ~/catkin_ws/devel/setup.bash \
-# && cd build && ./${RUN_CMD}
+&& cd build && ${RUN_CMD}
 " C-m C-m
 exit
 

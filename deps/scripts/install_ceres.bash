7eab9e3d9e581849d75935a07814fcb35aefedd4
Chris Choi <chutsu@gmail.com>
2022-03-22 09:46:11 +0000

    Fix ceres install script

--- deps/scripts/install_ceres.bash
+++ deps/scripts/install_ceres.bash
@@ -25,36 +25,6 @@ git checkout ${VERSION} --quiet
 mkdir -p build
 cd build
 cmake .. \
-
-# Clone
-if [ ! -d $INSTALL_PREFIX/src/ceres-solver ]; then
-  cd $INSTALL_PREFIX/src
-  git clone --quiet https://github.com/ceres-solver/ceres-solver
-  cd ..
-fi
-
-# Build
-cd $INSTALL_PREFIX/src/ceres-solver
-git checkout ${VERSION} --quiet
-
-mkdir -p build
-cd build
-cmake .. \
-
-# Clone
-if [ ! -d $INSTALL_PREFIX/src/ceres-solver ]; then
-  cd $INSTALL_PREFIX/src
-  git clone --quiet https://github.com/ceres-solver/ceres-solver
-  cd ..
-fi
-
-# Build
-cd $INSTALL_PREFIX/src/ceres-solver
-git checkout ${VERSION} --quiet
-
-mkdir -p build
-cd build
-cmake .. \
   -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
   -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
   -DCMAKE_PREFIX_PATH=$INSTALL_PREFIX

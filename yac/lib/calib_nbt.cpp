e8f5e2a5c7a9e7a0eb4fe94e1bfcc0600a275362
Chris Choi <chutsu@gmail.com>
2022-05-02 15:55:19 +0100

    Update calib_nbt

--- yac/lib/calib_nbt.cpp
+++ yac/lib/calib_nbt.cpp
@@ -738,14 +738,12 @@ void simulate_imu(const timestamp_t &ts_start,
                   vec3s_t &imu_gyro,
                   mat4s_t &imu_poses,
                   vec3s_t &imu_vels) {
-  const timestamp_t imu_dt = sec2ts(1.0 / imu_params.rate);
-  const timestamp_t dt = (1 / imu_params.rate) * 1e9;
-
   mat4_t T_WS = traj.get_pose(0);
   vec3_t r_WS = tf_trans(T_WS);
   mat3_t C_WS = tf_rot(T_WS);
   vec3_t v_WS = traj.get_velocity(0);
 
+  const timestamp_t dt = sec2ts(1.0 / imu_params.rate);
   timestamp_t ts_k = ts_start;
   while (ts_k <= ts_end) {
     // Get sensor pose, acceleration and angular velocity
@@ -755,26 +753,22 @@ void simulate_imu(const timestamp_t &ts_start,
     const vec3_t w_WS_W = traj.get_angular_velocity(ts_k);
 
     // Transform acceleration and angular velocity to body frame
-    const vec3_t g{0.0, 0.0, imu.g};
+    const vec3_t g{0.0, 0.0, imu_params.g};
     const vec3_t a_WS_S = C_WS.transpose() * (a_WS_W + g);
     const vec3_t w_WS_S = C_WS.transpose() * w_WS_W;
 
     // Propagate simulated ideal IMU measurements
     const real_t dt_s = ts2sec(dt);
     const real_t dt_s_sq = dt_s * dt_s;
-    const vec3_t g_W{0.0, 0.0, -imu.g};
+    const vec3_t g_W{0.0, 0.0, -imu_params.g};
     // -- Position at time k
-    const vec3_t b_a = ones(3, 1) * imu.b_a;
-    const vec3_t n_a = ones(3, 1) * imu.sigma_a_c;
     r_WS += v_WS * dt_s;
     r_WS += 0.5 * g_W * dt_s_sq;
-    r_WS += 0.5 * C_WS * (a_WS_S - b_a - n_a) * dt_s_sq;
+    r_WS += 0.5 * C_WS * a_WS_S * dt_s_sq;
     // -- velocity at time k
-    v_WS += C_WS * (a_WS_S - b_a - n_a) * dt_s + g_W * dt_s;
+    v_WS += C_WS * a_WS_S * dt_s + g_W * dt_s;
     // -- Attitude at time k
-    const vec3_t b_g = ones(3, 1) * imu.b_g;
-    const vec3_t n_g = ones(3, 1) * imu.sigma_g_c;
-    C_WS = C_WS * lie::Exp((w_WS_S - b_g - n_g) * ts2sec(dt));
+    C_WS = C_WS * lie::Exp(w_WS_S * ts2sec(dt));
     // -- Normalize rotation
     quat_t q = quat_t{C_WS};
     q.normalize();
@@ -907,7 +901,7 @@ int nbt_eval(const lissajous_traj_t &traj,
              matx_t &calib_covar) {
   // Setup
   const timestamp_t ts_start = traj.ts_start;
-  const timestamp_t ts_end = traj.ts_end;
+  const timestamp_t ts_end = ts_start + sec2ts(traj.T);
   const double cam_rate = calib.get_camera_rate();
 
   // Make a copy of the calibrator
@@ -1046,7 +1040,7 @@ int nbt_find(const ctrajs_t &trajs,
 
 int nbt_find(const lissajous_trajs_t &trajs,
              const calib_vi_t &calib,
-             const bool verbose = false) {
+             const bool verbose) {
   // Calculate info_k
   matx_t calib_covar;
   if (calib.recover_calib_covar(calib_covar) != 0) {

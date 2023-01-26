927942121605f8844709b7d1c2e00d885199bfb0
Chris Choi <chutsu@gmail.com>
2023-01-26 12:57:13 +0000

    Update

--- yac/demo/calib_euroc.cpp
+++ yac/demo/calib_euroc.cpp
@@ -162,7 +162,7 @@ void calibrate_imu(const std::string &data_path,
 int main(int argc, char *argv[]) {
   UNUSED(argc);
 
-  // calibrate_cameras(CAMERA_DATA, CAMERA_RESULTS_PATH);
+  calibrate_cameras(CAMERA_DATA, CAMERA_RESULTS_PATH);
   calibrate_imu(IMU_DATA, CAMERA_RESULTS_PATH, IMU_RESULTS_PATH);
   return 0;
 }

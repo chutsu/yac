37700b3aeafa5eb51644c8a48926b2ececb75ce8
Chris Choi <chutsu@gmail.com>
2024-09-05 01:52:45 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 set -e
-make debug
+
+TARGET="dev"
+CMD="make debug && ./lib/build/TestAprilGrid"
+tmux send-keys -t $TARGET -R C-l C-m
+tmux send-keys -t $TARGET -R "cd ~/projects/yac && clear && $CMD" C-m C-m
+exit

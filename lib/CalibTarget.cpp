1a2f19b05005c7de36772a4bec675a07ca5ff572
Chris Choi <chutsu@gmail.com>
2024-09-07 16:45:30 +0100

    Update CalibTarget

--- lib/CalibTarget.cpp
+++ lib/CalibTarget.cpp
@@ -13,4 +13,28 @@ int CalibTarget::getNumRows() const { return num_rows_; }
 
 int CalibTarget::getNumCols() const { return num_cols_; }
 
+// int CalibTarget::estimate(const CameraModel *cam,
+//                         const int cam_res[2],
+//                         const vecx_t &cam_params,
+//                         mat4_t &T_CF) const {
+//
+//
+//   // Check if we actually have data to work with
+//   if (nb_detections == 0) {
+//     return -1;
+//   }
+//
+//   // Create object points (counter-clockwise, from bottom left)
+//   vec2s_t img_pts;
+//   vec3s_t obj_pts;
+//   for (int i = 0; i < (tag_rows * tag_cols * 4); i++) {
+//     if (data(i, 0) > 0) {
+//       img_pts.emplace_back(data(i, 1), data(i, 2));
+//       obj_pts.emplace_back(data(i, 3), data(i, 4), data(i, 5));
+//     }
+//   }
+//
+//   return solvepnp(cam, cam_res, cam_params, img_pts, obj_pts, T_CF);
+// }
+
 } // namespace yac

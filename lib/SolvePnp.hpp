13039347c4a6e401a098b265672df5a49696714c
Chris Choi <chutsu@gmail.com>
2024-09-20 23:37:37 +0100

    Update SolvePnp

--- lib/SolvePnp.hpp
+++ lib/SolvePnp.hpp
@@ -2,6 +2,7 @@
 #include <ceres/ceres.h>
 
 #include "Core.hpp"
+#include "CalibTarget.hpp"
 #include "CameraGeometry.hpp"
 
 namespace yac {
@@ -20,6 +21,27 @@ public:
   int estimate(const vec2s_t &keypoints,
                const vec3s_t &object_points,
                mat4_t &T_camera_object);
+
+  static int estimate(const std::shared_ptr<CameraGeometry> &camera_geometry,
+                      const std::shared_ptr<CalibTarget> &calib_target,
+                      mat4_t &T_camera_target) {
+    // Get calibration target measurements
+    std::vector<int> tag_ids;
+    std::vector<int> corner_indicies;
+    vec2s_t keypoints;
+    vec3s_t object_points;
+    calib_target->getMeasurements(tag_ids,
+                                  corner_indicies,
+                                  keypoints,
+                                  object_points);
+    if (keypoints.size() < 10) {
+      return -1;
+    }
+
+    // Estimate relative pose
+    SolvePnp pnp{camera_geometry};
+    return pnp.estimate(keypoints, object_points, T_camera_target);
+  }
 };
 
 } // namespace yac

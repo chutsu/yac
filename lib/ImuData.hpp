4c2f58b59cc4b98e7a52d92ade1ad7c47dc3ff5b
Chris Choi <chutsu@gmail.com>
2024-09-22 00:42:26 +0100

    Add ImuData

---
+++ lib/ImuData.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include "Core.hpp"
+
+namespace yac {
+
+class ImuData {
+private:
+  std::vector<timestamp_t> timestamps_;
+  std::vector<vec3_t> acc_data_;
+  std::vector<vec3_t> gyr_data_;
+
+public:
+  ImuData() = default;
+  virtual ~ImuData() = default;
+
+  int getNumMeasurements() const { return timestamps_.size(); }
+
+  /** Get timestamp */
+  timestamp_t getTimestamp(const int index) const {
+    return timestamps_.at(index);
+  }
+
+  /** Get accelerometer measurement */
+  vec3_t getAcc(const int index) const { return acc_data_.at(index); }
+
+  /** Get gyroscope measurement */
+  vec3_t getGyr(const int index) const { return gyr_data_.at(index); }
+
+  /** Add measurement */
+  void add(const timestamp_t ts, const vec3_t &acc, const vec3_t gyr) {
+    timestamps_.push_back(ts);
+    acc_data_.push_back(acc);
+    gyr_data_.push_back(gyr);
+  }
+};
+
+} // namespace yac

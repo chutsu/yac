780fc094ec8e8f4354a628d8abbd5088ba4fde4e
Chris Choi <chutsu@gmail.com>
2024-09-25 00:24:52 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,9 +2,15 @@
 set -e
 
 TARGET="dev"
-# CMD="make release && gdb -ex=run -ex=bt -ex=quit ./build/TestCalibCamera"
-CMD="make debug && ./build/TestCameraResidual"
+# CMD="make debug && ./build/TestCalibCamera"
+# CMD="make debug && ./build/TestCameraChain"
 # CMD="make debug && ./build/TestCameraModel"
+CMD="make debug && ./build/TestInertialError"
+# CMD="make debug && ./build/TestReprojectionError"
+# CMD="make debug && ./build/TestSim"
 tmux send-keys -t $TARGET -R C-l C-m
 tmux send-keys -t $TARGET -R "cd ~/projects/yac && clear && $CMD" C-m C-m
 exit
+
+# python3 scripts/plot_sim.py
+# python3 scripts/plot_test_inertial_error_solve.py
